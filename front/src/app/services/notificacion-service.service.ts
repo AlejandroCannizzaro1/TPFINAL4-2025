48246fe615e0b0ea458d5e0f82071fee25cd4472
Alejandro <alejandrocannizzaro@gmail.com>
2025-11-10 02:05:09 -0300

    Agrego en el front la entidad notificacion y notificacionService

---
+++ front/src/app/services/notificacion-service.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, inject } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Notificacion } from "../entities/notificacion";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificacionService {
+  private readonly http = inject(HttpClient);
+  private readonly baseUrl = 'http://localhost:3001/notificaciones';
+
+  // Obtener notificaciones de un usuario
+  getNotificacionesByUsuario(idUsuario: number) {
+    return this.http.get<Notificacion[]>(
+      `${this.baseUrl}/usuario?idUsuario=${idUsuario}`
+    );
+  }
+
+  // Obtener notificaciones asociadas a un turno (solo admins)
+  getNotificacionesByTurno(idTurno: number) {
+    return this.http.get<Notificacion[]>(
+      `${this.baseUrl}/turno?idTurno=${idTurno}`
+    );
+  }
+}
