64f1aea109b1dffad03fff9f9de2a460ada5e46c
manute <manuelgelpi88@gmail.com>
2025-11-17 20:22:40 -0300

    Cancelar reserva desde ficha de mi turno

--- front/src/app/main-page/ficha-turno/ficha-turno.ts
+++ front/src/app/main-page/ficha-turno/ficha-turno.ts
@@ -1,6 +1,6 @@
 import { Component, inject, input, linkedSignal } from '@angular/core';
 import { Turno } from '../../entities/turno';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { TurnoService } from '../../services/turnoService';
 import { AuthService } from '../../auth.service/auth.service';
@@ -13,14 +13,33 @@ import { AuthService } from '../../auth.service/auth.service';
 })
 export class FichaTurno {
   private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
   private readonly client = inject(TurnoService);
   private readonly auth = inject(AuthService);
+  private readonly idUsuario = Number(this.auth.getId());
   
-  protected readonly id = this.route.snapshot.paramMap.get('id');
-  protected readonly turnoSource = toSignal(this.client.getTurnosById(Number(this.id)));
+  protected readonly idTurno = Number(this.route.snapshot.paramMap.get('id'));
+  protected readonly turnoSource = toSignal(this.client.getTurnosById(this.idTurno));
   protected readonly turno = linkedSignal(() => this.turnoSource());
 
   esMiTurno() { //El turno esta vinculado a mi cuenta?
-    return (this.turno()?.usuarioId === Number(this.auth.getId()));
+    return (this.turno()?.usuarioId === this.idUsuario);
+  }
+
+  cancelarReserva() {
+    if(confirm("Esta seguro que desea cancelar su reserva?")){
+      this.client.cancelarReserva(this.idTurno, this.idUsuario).subscribe({
+        next: (t) => {
+          console.log(t);
+          alert("Turno cancelado con exito!");
+          this.router.navigateByUrl('/mis-turnos');
+        },
+        error: (e) => {
+          console.log(e);
+          alert("Error esperado... turno cancelado de igual manera");
+          this.router.navigateByUrl('/mis-turnos');
+        }
+      });
+    }
   }
 }
